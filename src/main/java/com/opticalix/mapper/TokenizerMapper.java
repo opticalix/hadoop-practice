ae03de6158023e9d6e5ad8c7e95a0e638d847c73
Hongfei Ji <jihongfei@zepplabs.com>
2017-10-14 16:34:26 +0800

    Add hadoop word count jars

---
+++ src/main/java/com/opticalix/mapper/TokenizerMapper.java
@@ -0,0 +1,27 @@
+package com.opticalix.mapper;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * jar diff?
+ * 	hadoop-client
+ * 	common
+ * 	hdfs
+ */
+public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+    IntWritable one = new IntWritable(1);
+	Text word = new Text();
+	
+	public void map(Object key, Text value, Context context) throws IOException,InterruptedException {
+		StringTokenizer itr = new StringTokenizer(value.toString());
+		while(itr.hasMoreTokens()) {
+			word.set(itr.nextToken());
+			context.write(word, one);
+		}
+	}
+}
